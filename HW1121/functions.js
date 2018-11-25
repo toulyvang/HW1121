a7498de07ddcdbd0e166c873b7f250ebcac7b710
toulyvang <45182226+toulyvang@users.noreply.github.com>
2018-11-25 14:25:50 -0600

    Add files via upload

---
+++ HW1121/functions.js
@@ -0,0 +1,19 @@
+const addPic1 = () => {
+    let theDiv = document.getElementById("div1");
+    theDiv.style.backgroundImage = 'url(image-124.JPG)';
+}
+
+const addPic2 = () => {
+    let theDiv = document.getElementById("div2");
+    theDiv.style.backgroundImage = 'url(image-186.jpg)';
+}
+
+const addPic3 = () => {
+    let theDiv = document.getElementById("div3");
+    theDiv.style.backgroundImage = 'url(mustardd.JPG)';
+}
+
+const navfunction = () => {
+    let navs = document.getElementById("nav1");
+    navs.classList.add('navStyle');
+}
